ff411a40bc02058530738b71ef0a60108e953a9c
AhmedOmar <ahmedomarpro@Gmail.com>
2022-11-16 20:31:03 +0200

    Continuous-Integration

--- app/src/main/java/com/ao/staticanalysis/MainActivity.kt
+++ app/src/main/java/com/ao/staticanalysis/MainActivity.kt
@@ -2,24 +2,13 @@ package com.ao.staticanalysis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//Toast.makeText(this, )
-
-
-
-
-
-
-
-
-
-
-
-
+    Toast.makeText (this,"Welcome_Continuous-Integration" ,Toast.LENGTH_LONG).show()
 
     }
 }
